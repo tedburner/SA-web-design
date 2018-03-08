a579da3bbb4b65ab1be5497a71229e2e9b05489e
lingjun.jlj <lingjun.jlj@120yibao.com>
2018-03-08 17:33:50 +0800

    fix

--- code/input_sentence.py
+++ code/input_sentence.py
@@ -19,12 +19,15 @@ MAX_NB_WORDS = 50000  # 处理的最大单词数量
 # 手动输入句子分析
 def input_sentence(INPUT_SENTENCE):
     model = load_model('../data/train_model.h5')
-    print INPUT_SENTENCE
+    return predict(INPUT_SENTENCE,model)
+
+
+def predict(text,model):
     word2index, vocab_size, num_recs, word_freqs = word_to_index(0, 0, collections.Counter())
     # 使用numpy生成一个list的数组
     input_lsit = np.empty(1, dtype=list)
     # 使用nltk句子分词
-    words = nltk.word_tokenize(INPUT_SENTENCE.lower())
+    words = nltk.word_tokenize(text.lower())
     seq = []
     for word in words:
         if word in word2index:
@@ -38,22 +41,7 @@ def input_sentence(INPUT_SENTENCE):
     return label2word[labels[0]]
 
 
-def predict_proba(texts):
-    model = load_model('../data/train_model.h5')
-    tokenizer = Tokenizer(num_words=MAX_NB_WORDS)
-    tokenizer.fit_on_texts(texts)
-    # sequences = tokenizer.texts_to_sequences(texts)
-    encoded = tokenizer.texts_to_matrix([texts], mode='freq')
-    # data = pad_sequences(encoded, maxlen=MAX_SENTENCE_LENGTH)
-    #    data=one_hot(texts, 4)
-    # return model.predict_proba(data, verbose=0)
-    return model.predict(encoded, verbose=0)
-
-
 if __name__ == '__main__':
-    word = 'I love you'
-    # clean_doc(doc)
-    # list1 = ['I like read.', 'The good is bad', 'He is not good']
-    # list2=GetDict.readDict(GetDict.getDict('test_word.csv')).values()
+    word = ''
     result = input_sentence(word)
     print result
