cd8800dae078f51f7cf36f7bd065157ea553fb40
lingjun.jlj <lingjun.jlj@120yibao.com>
2018-03-21 16:20:46 +0800

    fix

--- test/main.py
+++ test/main.py
@@ -9,14 +9,16 @@ from keras.preprocessing.text import Tokenizer
 from code.train import *
 
 if __name__ == '__main__':
-    input = 'I like read.'
-    texts = [input]
-    input_lsit = np.empty(1, dtype=list)
+    texts = ['I like read.', 'Well done!']
     model = load_model(path + '/data/model.h5')
     tokenizer = Tokenizer()
     tokenizer.fit_on_texts(texts)
-    input_lsit = tokenizer.text_to_sequences(texts)
-    result = sequence.pad_sequences(input_lsit, maxlen=MAX_SENTENCE_LENGTH)
+    vocab_size = len(tokenizer.word_index) + 1
+    encoded_docs = tokenizer.texts_to_sequences(texts)
+    result = sequence.pad_sequences(encoded_docs, maxlen=MAX_SENTENCE_LENGTH)
+    print result
+    print model.predict(result)
     labels = [int(round(x[0])) for x in model.predict(result)]
     label2word = {1: '积极', 0: '消极'}
-    print label2word[labels[0]]
+    for i in range(len(texts)):
+        print('{}   {}'.format(label2word[labels[i]], texts[i]))
